8a8d3fb86aa07373e4af074ee77b485010c807b5
hungwang712 <nguyenquanghung51207@gmail.com>
2024-12-19 11:40:20 +0700

    Update script.js
    Signed-off-by: hungwang712 <nguyenquanghung51207@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showForgiveness() {
     const forgivenessMessage = document.getElementById("forgiveness-message");
     forgivenessMessage.textContent = "Cảm ơn cậu , tớ hứa sẽ không có lần sauuu";
-    forgivenessMessage.classList.add("forgiveness-message-visible");
+    forgivenessMessage.classList.add("forgiveness-message");
 }
